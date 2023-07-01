d30c3b23ca49e7e7a5da3434dbb4fe2311d30798
theasmit <asmit@amberflux.com>
2023-07-01 18:43:49 +0530

    Upload app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
---
+++ lib/mainpage.dart
@@ -0,0 +1,389 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//Colors for bg
+var defaultBg = const Color.fromARGB(255, 246, 247, 246);
+var underweight = const Color.fromARGB(255, 134, 177, 227);
+var normal = const Color.fromARGB(255, 193, 232, 152);
+var overweight = const Color.fromARGB(255, 243, 138, 140);
+
+//color for button
+var buttoncolor = const Color.fromARGB(255, 140, 140, 210);
+var resbutton = const Color.fromARGB(255, 244, 244, 245);
+
+//color for text
+var nortext = Colors.black87;
+var overtext = Colors.white;
+
+//starting of main page
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  //current colors to set in page
+  var currbg = defaultBg;
+  var currbutton = buttoncolor;
+  var currtext = nortext;
+  var cal = overtext;
+
+  //strings which will change over time
+  String title = 'BMI App';
+  String subtitle = 'Calculate your BMI';
+
+  //variable used in page
+  late int? result;
+  late double res;
+  late double? w;
+  late double? h;
+  String dropdownValue = 'Male';
+  //controllers for input
+  final _weightcontroller = TextEditingController();
+  final _heightcontroller = TextEditingController();
+  final _agecontroller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: currbg,
+      body: Container(
+        padding: const EdgeInsets.all(18.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 0,
+              ),
+              Text(title,
+                  style: GoogleFonts.lato(
+                    fontSize: 70,
+                    fontWeight: FontWeight.w500,
+                    color: currtext,
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                subtitle,
+                style: GoogleFonts.lato(
+                  fontSize: 26,
+                  fontWeight: FontWeight.normal,
+                  color: currtext,
+                ),
+              ),
+              const SizedBox(
+                height: 75,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _agecontroller,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        label: Text(
+                          'Age',
+                          style:
+                              GoogleFonts.lato(color: currtext, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 18),
+                      child: DropdownButton<String>(
+                        alignment: Alignment.center,
+                        isExpanded: true,
+                        value: dropdownValue,
+                        items: <String>['Male', 'Female', 'Others']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: GoogleFonts.lato(
+                                  color: nortext, fontSize: 20),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(
+                            () {
+                              dropdownValue = newValue!;
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: _weightcontroller,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        label: Text(
+                          'Weight(kg)',
+                          style: GoogleFonts.lato(
+                            color: currtext,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: _heightcontroller,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: currtext),
+                        ),
+                        label: Text(
+                          'Height(cm)',
+                          style: GoogleFonts.lato(
+                            color: currtext,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Center(
+                child: SizedBox(
+                  height: 50,
+                  width: 250.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: currbutton,
+                    ),
+                    //processing of data
+                    onPressed: () {
+                      if (_agecontroller.text.isEmpty ||
+                          _heightcontroller.text.isEmpty ||
+                          _weightcontroller.text.isEmpty) {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(24.0))),
+                            title: const Text("Hey!"),
+                            content: const Text("You forgot to fill something"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: currbg,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(14.0))),
+                                  // color: currbg,
+                                  padding: const EdgeInsets.all(14),
+                                  child: const Text(
+                                    "Okay",
+                                    style: TextStyle(color: Colors.black87),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (_agecontroller.text.startsWith('-') ||
+                          _agecontroller.text == '0') {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(24.0))),
+                            title: const Text("Hey!"),
+                            content: const Text("Something wrong with the age"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: currbg,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(14.0))),
+                                  // color: currbg,
+                                  padding: const EdgeInsets.all(14),
+                                  child: const Text(
+                                    "Okay!",
+                                    style: TextStyle(color: Colors.black87),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (_weightcontroller.text.startsWith('-') ||
+                          _weightcontroller.text == '0') {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(24.0))),
+                            title: const Text("Hey!"),
+                            content:
+                                const Text("Something wrong with the weight"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: currbg,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(14.0))),
+                                  // color: currbg,
+                                  padding: const EdgeInsets.all(14),
+                                  child: const Text(
+                                    "Okay!",
+                                    style: TextStyle(color: Colors.black87),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (_heightcontroller.text.startsWith('-') ||
+                          _heightcontroller.text == '0') {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(24.0))),
+                            title: const Text("Hey!"),
+                            content: const Text("Something wrong with height"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: currbg,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(14.0))),
+                                  // color: currbg,
+                                  padding: const EdgeInsets.all(14),
+                                  child: const Text(
+                                    "Okay!",
+                                    style: TextStyle(color: Colors.black87),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      //if everything works fine then, app shows the result[BMI]
+                      else {
+                        setState(() {
+                          w = double.tryParse(_weightcontroller.text);
+                          h = double.tryParse(_heightcontroller.text);
+                          res = calc(w!, h!);
+                        });
+                        if (res < 18.5) {
+                          //underweight
+                          setState(() {
+                            cal = nortext;
+                            currtext = overtext;
+                            currbg = underweight;
+                            currbutton = resbutton;
+                            title = res.toInt().toString();
+                            subtitle = 'Time to eat some healthy snacks!';
+                          });
+                        } else if (res > 25) {
+                          //overweight
+                          setState(() {
+                            cal = nortext;
+                            currtext = overtext;
+                            currbg = overweight;
+                            currbutton = resbutton;
+                            title = res.toInt().toString();
+                            subtitle =
+                                'Break free from excess weight and discover a lighter life';
+                          });
+                        } else {
+                          //normal
+                          setState(() {
+                            cal = nortext;
+                            currbg = normal;
+                            currbutton = resbutton;
+                            title = res.toInt().toString();
+                            subtitle = 'You\'re doing well!';
+                          });
+                        }
+                      }
+                    },
+                    child: Text(
+                      'Calculate',
+                      style: GoogleFonts.lato(fontSize: 26, color: cal),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//function to calculate BMI
+double calc(double w, double h) {
+  double l = h / 100;
+  double bmi = w / (l * l);
+  return bmi;
+}
